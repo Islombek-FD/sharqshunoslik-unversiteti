70264d562002bbc6151ad3c4103998682885ff93
Islombek-FD <islombekqurbonovfd@gmail.com>
2021-10-21 23:09:42 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const elWrapper = document.querySelector('.wrapper');
 const elHamburgerMenu = document.querySelector('.hamburger__menu');
+const elMenu = document.querySelector('.menu');
 
+
+// Listener parent element click event
 elWrapper.addEventListener('click', (evt) => {
     const isHamburgerMenu = evt.target.matches('.hamburger__menu') || evt.target.matches('.hamburger__icon');
 
+    // Open or Close Hamburger Menu
     if (isHamburgerMenu) {
         elHamburgerMenu.classList.toggle('hamburger__menu--open');
+        elMenu.classList.toggle('menu--open');
+    }
+    // Open or Close Sub Menu
+    else if (evt.target.matches('.menu__link')) {
+        evt.target.classList.toggle('menu__link--click');
     }
 })
