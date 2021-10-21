a82c2e0b07cbcafcccc5f5e218bc32539566f723
Islombek-FD <islombekqurbonovfd@gmail.com>
2021-10-21 16:35:09 +0300

    New commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
-const elMain = document.querySelector('.main');
-// const elMenuLinkAll = document.querySelectorAll('.menu__link');
-// const elSubMenuAll = document.querySelectorAll('.sub-menu');
-const elSiteMenu = document.querySelector('.site__menu');
-const elSiteInfo = document.querySelector('.site__info');
+const elWrapper = document.querySelector('.wrapper');
+const elHamburgerMenu = document.querySelector('.hamburger__menu');
 
-elMain.addEventListener('click', (evt) => {
-    if (evt.target.matches('.menu__link')) {
-        evt.target.classList.toggle('menu__link--open');
-    }
-    else if (evt.target.matches('.hamburger__menu') || evt.target.matches('.hamburger__icon')) {
-        elSiteMenu.classList.toggle('site__menu--open');
-        elSiteInfo.classList.toggle('site__info--fluid');
+elWrapper.addEventListener('click', (evt) => {
+    const isHamburgerMenu = evt.target.matches('.hamburger__menu') || evt.target.matches('.hamburger__icon');
+
+    if (isHamburgerMenu) {
+        elHamburgerMenu.classList.toggle('hamburger__menu--open');
     }
 })
-
-// elMenuLinkAll.forEach ((item , i) => item.dataset.menuLinkId = i);
-// elSubMenuAll.forEach((e, i) => e.dataset.subMenuId = i);
